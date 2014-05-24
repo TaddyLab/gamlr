30d8dc628f94a0995239f5c439c7326c6265a8f3
Nelson Auner <ne.auner@gmail.com>
2014-05-23 22:21:44 -0500

    reverted changes to master branch and added R source code to pygamlr branch

---
+++ src/R/Rversion.h
@@ -0,0 +1,25 @@
+/* Rversion.h.  Generated automatically. */
+#ifndef R_VERSION_H
+#define R_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define R_VERSION 196610
+#define R_NICK "Frisbee Sailing"
+#define R_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
+#define R_MAJOR  "3"
+#define R_MINOR  "0.2"
+#define R_STATUS ""
+#define R_YEAR   "2013"
+#define R_MONTH  "09"
+#define R_DAY    "25"
+#define R_SVN_REVISION 63987
+#define R_FILEVERSION    3,02,63987,0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* not R_VERSION_H */
--- src/lhd.c
+++ src/lhd.c
@@ -2,7 +2,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <Rmath.h>
+#include "Rmath.h"
 #include "lhd.h"
 #include "vec.h"
 
